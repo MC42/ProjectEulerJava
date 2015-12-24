3b93f267be7d9965913f8b1d10249ef24aa1a34f
MC42 <tjflynn@theflynn.org>
2015-12-24 13:06:12 -0500

    Comments in Euler 01.

--- Euler_01.java
+++ Euler_01.java
@@ -1,20 +1,19 @@
 public class Euler_01 {
 public static long count = 0;
 public static void main(String[] args) {
-for (int i = 0; i < 1000; i++)
+for (int i = 0; i < 1000; i++) //Creates a loop in the requested range.  0 to 1000.
 
-{
-if (i % 3 == 0)
-{
-count = count + i;
-}
+	{
+	if (i % 3 == 0) //If divisible by 3, add to total/
+		{
+		count = count + i;
+		}
 
-else if (i % 5 == 0)
-{
-count = count + i;
-}
-}
-
-System.out.println(count);
-}
+	else if (i % 5 == 0) //if divisible by 5, add to total.
+		{
+		count = count + i;
+		}
+	}
+	System.out.println(count); //When done, print total.
+	}
 }
